4612eacf73ce350c0d6b34d7057b942365cf29ce
Fernando De Leon Gil <gasnare@hotmail.com>
2024-11-21 17:27:37 -0500

    Add basic agents and header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import Agents from "./components/Agents";
 import { Header } from "./components/Header";
 import "./styles/App.css";
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+      <Agents />
+    </main>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
 import Valorant from "../assets/valorant.png";
+import "../styles/Header.css";
 
 export const Header = () => {
   return (
     <header>
-      <div>
+      <div className="header-logo">
         <img src={Valorant} alt="Icon of Valorant" />
       </div>
       <ol>
         <li>Agents</li>
         <li>Maps</li>
+        <li>Ranks</li>
       </ol>
     </header>
   );
