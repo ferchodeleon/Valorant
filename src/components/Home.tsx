efd69e3ac66c0028d38f5044b6328c1d56dc916f
Fernando De Leon Gil <gasnare@hotmail.com>
2024-12-20 15:25:22 -0500

    Create Component page of Home

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Valorant</h1>
+      <p>
+        Hola aquí podrás obtener diferentes datos del juego Valorant, selecciona
+        la opción deseada
+      </p>
+    </div>
+  );
+};
+
+export default Home;
