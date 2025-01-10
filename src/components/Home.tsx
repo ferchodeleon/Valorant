a59d3a3444232f8028e10c63c452d7fab50de480
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-10 11:38:31 -0500

    Add video background and description page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,26 @@
+import "../styles/Home.css";
+import VideoBackground from "../assets/videos/video-background.mp4";
+
 const Home = () => {
   return (
-    <div>
-      <p>
-        Hola aquí podrás obtener diferentes datos del juego Valorant, selecciona
-        la opción deseada
-      </p>
-    </div>
+    <section className="home-container">
+      <video
+        src={VideoBackground}
+        autoPlay
+        preload="metadata"
+        loop
+        muted
+        playsInline
+        className="home-video"
+      />
+      <div className="home-content">
+        <p className="home-title">Bienvenido a Valorant API</p>
+        <p className="home-description">
+          Hola aquí podrás obtener diferentes datos del juego Valorant,
+          selecciona la opción deseada
+        </p>
+      </div>
+    </section>
   );
 };
 
