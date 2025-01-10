ccba4f54b37db40fe8e6200f0b0fd44b2c44650b
Fernando De Leon Gil <gasnare@hotmail.com>
2025-01-10 16:27:26 -0500

    Add params to language and duplicate sova agent

--- src/services/agents.http.service.ts
+++ src/services/agents.http.service.ts
@@ -1,8 +1,8 @@
 import { AgentModels } from "../models/Agents.model";
-import { BaseHttpService } from "./base.http.service";
+import BaseHttpService from "./base.http.service";
 
 export class AgentHttpService {
-  private base = new BaseHttpService<AgentModels>("agents");
+  private base = new BaseHttpService<AgentModels>("agents", true);
 
   async getAllAgents(): Promise<AgentModels[]> {
     const data = await this.base.getAll();
--- src/services/base.http.service.ts
+++ src/services/base.http.service.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
 
-export class BaseHttpService<T> {
+class BaseHttpService<T> {
   feature: string;
-  constructor(feature: string) {
+  character = false;
+  constructor(feature: string, character: boolean) {
     this.feature = feature;
+    this.character = character;
   }
   private url: string = import.meta.env.VITE_API_URL;
 
   async getAll() {
     const { data } = await axios.get<{ data: T[] }>(
-      `${this.url}${this.feature}`
+      `${this.url}${this.feature}`,
+      { params: { language: "es-MX", isPlayableCharacter: this.character } }
     );
     return data.data;
   }
 }
+
+export default BaseHttpService;
