88fdac67fad17f96fa3717426842fda95ae77650
Fernando De Leon Gil <gasnare@hotmail.com>
2025-04-30 10:28:39 -0500

    support languages

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import Agents from "./components/Agents";
 import { Header } from "./components/Header";
 import Home from "./components/Home";
 import "./styles/App.css";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./translation";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/agents" element={<Agents />} />
-        </Routes>
-      </main>
-    </Router>
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/agents" element={<Agents />} />
+          </Routes>
+        </main>
+      </Router>
+    </I18nextProvider>
   );
 }
 
